9bf239d1b8ee7e2eae0bb4e60a40f610dd416f31
donceykong <donceyalbin@gmail.com>
2025-04-04 21:28:53 -0600

    Added path vars

--- dataset_tools/ros1/docker/run_and_enter_container.bash
+++ dataset_tools/ros1/docker/run_and_enter_container.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# Give privledge for screen sharing
 xhost +local:root
+
+# Resolve absolute paths
+DATASETS_DIR="$(realpath /home/donceykong/Datasets)"
+SCRIPTS_DIR="$(realpath /home/donceykong/Desktop/ARPG/projects/CU-Multi/dataset_tools/ros1/conversion_scripts)"
 
 # Run Docker container with specified configurations
 docker run -it -d --rm --privileged \
@@ -9,6 +14,8 @@ docker run -it -d --rm --privileged \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="$DATASETS_DIR:/root/Datasets:rw" \
+  --volume="$SCRIPTS_DIR:/root/conversion_scripts:rw" \
   cu_multi_ros1
 
 # Optional mounts and GPU support (uncomment as needed):
