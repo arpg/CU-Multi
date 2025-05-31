59f15ef0c12caa4b7ead1a13f180dfdc21fdacd5
donceykong <donceyalbin@gmail.com>
2025-05-31 16:46:37 -0600

    Added instructions.

--- dataset_tools/ros2/docker/run_and_enter_container.bash
+++ dataset_tools/ros2/docker/run_and_enter_container.bash
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# Resolve absolute paths
-DATASETS_DIR="$(realpath /home/donceykong/Datasets)"
-SCRIPTS_DIR="$(realpath /home/donceykong/Desktop/ARPG/projects/CU-Multi/dataset_tools/ros2/conversion_scripts)"
+# Resolve absolute paths to:
+
+# 1. Dir containing CU-Multi Dataset
+DATASETS_DIR="$(realpath <ABS-PATH-TO>/Datasets)"
+# 2. CU-Multi/dataset_tools/ros2/conversion_scripts
+SCRIPTS_DIR="$(realpath /<ABS-PATH-TO>/CU-Multi/dataset_tools/ros2/conversion_scripts)"
+# 3. ROS2 WS, in case you would like to test methods on the CU-Multi Dataset ;)
+ROS2_WS_DIR="$(realpath <ABS-PATH-TO>/<your_ros2_ws>)"
 
 # Give privledge for screen sharing
 xhost +local:root
@@ -16,6 +21,7 @@ docker run -it -d --rm --privileged \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="$DATASETS_DIR:/root/Datasets:rw" \
   --volume="$SCRIPTS_DIR:/root/conversion_scripts:rw" \
+  --volume="$ROS2_WS_DIR:/root/<your_ros2_ws>:rw" \
   cu_multi_ros2
 
 # Optional mounts and GPU support (uncomment as needed):
